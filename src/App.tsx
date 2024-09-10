9b0241c88f27a168f5465515dccf11770a9d0915
kerry-ama <kaowusu03@gmail.com>
2024-09-10 00:11:08 -0500

    created remaining form tags and anchor tags. created kanbas directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
 import Labs from './Labs';
+import { HashRouter, Route, Routes, Navigate} from "react-router-dom";
 
 function App() {
   return (
+    <HashRouter>
     <div className="App">
       <h1>Welcome to Web Dev</h1>
-      <Labs />
+      <h2>Kerry Owusu</h2>
+      <Routes>
+        <Route path="/" element={<Navigate to="Labs" />} />
+        <Route path="/Labs/*" element={<Labs />} />
+      </Routes>
     </div>
+    </HashRouter>
   );
 }
 
