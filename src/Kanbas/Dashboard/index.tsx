dc9a453a8f900ee16ab8691a63cecc5a01120d09
kerry-ama <kaowusu03@gmail.com>
2024-09-19 20:52:11 -0500

    made few modifications

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -4,8 +4,9 @@ export default function Dashboard() {
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
       <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
-      <div id="wd-dashboard-courses">
-        <div className="wd-dashboard-course">
+      <div id="wd-dashboard-courses" className="row">
+        <div className="row row-col1 row-cols-md-5 g-4">
+          <div className="wd-dashboard-course col" style={{ width: "300px"}}>
           <img src="/images/reactjs.jpg" width={200} />
           <div>
             <Link className="wd-dashboard-course-link"
@@ -17,7 +18,8 @@ export default function Dashboard() {
             </p>
             <Link to="/Kanbas/Courses/1234/Home"> Go </Link>
           </div>
-        </div>
+          </div>
+        
         <div className="wd-dashboard-course"> 
           <img src="/images/nodejs.jpg" width={200} />
           <div>
@@ -108,6 +110,7 @@ export default function Dashboard() {
             </p>
             <Link to="Kanbas/Courses/4000/Home"> Go </Link>
           </div>
+        </div>
         </div>
       </div>
     </div>
