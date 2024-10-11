a49ed736a4a37ae7b53b1ed7732421ec03b070e5
kerry-ama <kaowusu03@gmail.com>
2024-10-11 13:32:26 -0500

    start of lab 3

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,7 +1,25 @@
+import BooleanVariables from "./BooleanVariables";
+import TernaryOperator from "./TernaryOperator";
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+import IfElse from "./IfElse";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInline";
+import LegacyFunctions from "./LegacyFunctions";
+
+
 export default function Lab3() {
     return (
       <div>
         <h2>Lab 3</h2>
+        <VariablesAndConstants />
+        <VariableTypes />
+        <BooleanVariables />
+        <IfElse />
+        <TernaryOperator />
+        <ConditionalOutputIfElse />
+        <ConditionalOutputInline />
+        <LegacyFunctions />
       </div>
     );
   }
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import './index.css';
-import App from './App';
+import App from './App';  // imports from App.tsx. The .tsx extension is implied
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
