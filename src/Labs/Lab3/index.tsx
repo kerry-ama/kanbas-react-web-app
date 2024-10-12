f0f524e4a28cd8f6680da9cba5dd97307dacc6d3
kerry-ama <kaowusu03@gmail.com>
2024-10-11 19:30:05 -0500

    completed tasks up to 2.4.13

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -6,7 +6,24 @@ import IfElse from "./IfElse";
 import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
 import ConditionalOutputInline from "./ConditionalOutputInline";
 import LegacyFunctions from "./LegacyFunctions";
-
+import ArrowFunctions from "./ArrowFunction";
+import ImpliedReturn from "./ImpliedReturn";
+import TemplateLiterals from "./TemplateLiterals";
+import SimpleArrays from "./SimpleArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ForLoops from "./ForLoops";
+import MapFunction from "./MapFunction";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import FilterFunction from "./FilterFunction";
+import JsonStringify from "./JsonStringify";
+import House from "./House";
+import TodoItem from "./todos/TodoItem";
+import TodoList from "./todos/TodoList";
+import Spreading from "./Spreading";
+import Destructing from "./Destructing";
+import FunctionDestructing from "./FunctionDestructing";
 
 export default function Lab3() {
     return (
@@ -20,6 +37,24 @@ export default function Lab3() {
         <ConditionalOutputIfElse />
         <ConditionalOutputInline />
         <LegacyFunctions />
+        <ArrowFunctions />
+        <ImpliedReturn />
+        <TemplateLiterals />
+        <SimpleArrays />
+        <ArrayIndexAndLength />
+        <AddingAndRemovingToFromArrays />
+        <ForLoops />
+        <MapFunction />
+        <FindFunction />
+        <FindIndex />
+        <FilterFunction />
+        <JsonStringify />
+        <House />
+        <TodoItem />
+        <TodoList />
+        <Spreading />
+        <Destructing />
+        <FunctionDestructing />
       </div>
     );
   }
