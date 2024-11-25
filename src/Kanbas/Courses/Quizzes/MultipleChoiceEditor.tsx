b6d60e4a79c2ae8345847888410a1406f6b4a2a3
kerry-ama <kaowusu03@gmail.com>
2024-11-25 11:21:21 -0600

    edited quiz

--- src/Kanbas/Courses/Quizzes/MultipleChoiceEditor.tsx
+++ src/Kanbas/Courses/Quizzes/MultipleChoiceEditor.tsx
@@ -1,25 +1,62 @@
+import { useState } from "react";
 import TextEditor from "./TextEditor";
 
 export default function MultipleChoiceEditor() {
-    
+    const [title, setTitle] = useState("");
+    const [points, setPoints] = useState(0);
+    const [question, setQuestion] = useState("");
+    const [choices, setChoices] = useState([]);
+    const [correctChoice, setCorrectChoice] = useState(null);
+
     return (
         <div id="wd-mc-editor">
+            <div id="dropdown-container" className="dropdown-row">
             <div className="dropdown">
-          <label>
-            <select
-              defaultValue={"easy"}
-            >
-              <option value="easy">Easy</option>
-              <option value="medium">Medium</option>
-              <option value="hard">Hard</option>
-            </select>
-          </label>
-        </div>
-        <hr />
+                    <select
+                        defaultValue={"easy"}
+                    >
+                        <option value="easy">Easy</option>
+                        <option value="medium">Medium</option>
+                        <option value="hard">Hard</option>
+                    </select>
+               
+            </div>
+            <div className="dropdown">
+                <label>
+                    <select
+                        defaultValue={"Multiple Choice"}
+                    >
+                        <option value="mc">Multiple Choice</option>
+                        <option value="tf">True/False</option>
+                        <option value="blank">Fill in the Blank</option>
+                    </select>
+                </label>
+            </div>
+            <div className="end">
+          
+                <input
+                    type="number"
+                    value={points}
+                    onChange={(e) => setPoints(Number(e.target.value))}
+                    className="form-control"
+                />
+                </div>
+            </div>
+            <hr />
             <h6><strong>Question:</strong></h6>
-        <TextEditor />
+            <div className="mb-2">
+                <input 
+                    placeholder="Question Title"
+                    type="text"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    className="form-control"
+                />
+                </div>
+
+            <TextEditor />
 
         </div>
     )
-   
+
 }
